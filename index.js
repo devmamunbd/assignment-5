e34c83806a9503917e15836c88bc7fc9098e0e05
dymamunbd <mdmamunfedev@gmail.com>
2024-02-19 11:07:10 +0600

    js first commit

--- index.js
+++ index.js
@@ -1,33 +1,83 @@
-// let seatCount = 0;
-// let seatLeft = 0;
-
-// const seatList = document.querySelectorAll('.seats');
-
-// for(const seat of seatList) {
-//     seat.addEventListener('click', function() {
-//         seat.style.background = 'Green';
-//         seatCount += 1;
-//         seatLeft -= 1;
-//         console.log(seatCount)
+let seatCount = 0;
+let seatLeft = 8;
+let totalPrices = 0;
 
 
-//         // const setCount = document.querySelector('.seat-count').innerText;
-//         // // console.log(setCount)
+const seatList = document.querySelectorAll('.seats');
 
-//         // const card = price.parentNode.childNodes[2]; 
-//         // console.log(card)
-//         // const price = cart.getElementById('price').innerText;
+for(const seat of seatList) {
+    seat.addEventListener('click', function() {
+        seat.style.background = 'Green';
+        seatCount += 1;
+        seatLeft -= 1;
 
-//         // totalPrice('total-price', parseInt(price))
-//         // grandTotal('grand-total', parseInt(price))
-//         // console.log(totalCost, grandTotal)
-//         setInnerText('seat-increase', seatCount);
-//         setInnerText('seat-left', seatLeft);
-//     })
+      const seatName = document.querySelector('.seats').innerText;
+      const ecoNomy = document.querySelector('.economy').innerText;
+      const priceList = document.querySelector('.price-list').innerText;
+
+      // const carted = seat.childNodes[2].innerText;
+      // console.log(carted)
+      // const price = parseInt(carted.querySelector('#prices-list').innerText);
+      // console.log(price)
+      
+      const titleContainer = document.getElementById('seat-container');
+      const p = document.createElement('p')
+      p.innerText = seatName + " " + ecoNomy + " " + priceList;      
+      titleContainer.appendChild(p)      
+    
+      totalPrices += priceList;
+      document.getElementById('total-price').innerText = totalPrices;
+
+
+      document.getElementById('grand-total').innerText = totalPrices;
+      // totalPrice('total-price',parseInt(price))
+
+
+
+
+      
+        setInnerText('seat-increase', seatCount);
+        setInnerText('seat-left', seatLeft);
+    })
 
 
     
-// }
+}
+
+
+const btn = document.getElementById('apply-btn');
+
+btn.addEventListener('click', function () {
+  
+  const couponCode = document.getElementById('input-field').value;
+  const couponCodeText = couponCode.split(" ").join("").toUpperCase();
+  // console.log(couponCodeText)
+
+  if (couponCodeText === "NEW15") {
+    return ("15% Okay")
+  
+  }
+   else if (couponCodeText === "COUPLE20") {
+    return ("20% Okay")
+  }
+
+  else {
+    alert("invalid")
+  }
+
+
+})
+
+
+
+const nextBtn = document.getElementById('next-btn');
+
+nextBtn.addEventListener('click', function () {
+   const modal = document.getElementById('modal');
+   modal.classList.remove('hidden')
+})
+
+
 
 // function totalPrice(id, value){
 //   const totalPrice =  document.getElementById(id).innerText;
@@ -43,6 +93,6 @@
 //     setInnerText('grand-total', sum)
 //   }
 
-// function setInnerText(id, value) {
-//     document.getElementById(id).innerText = value;
-// }
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
